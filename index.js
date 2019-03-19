4033f9c2d8311fe8296f3ecac18476157058633d
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-19 14:39:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function yourAnimal() {var animal = 'cat';
   return animal
 }
 
-function add2(n){return n + two}
+function add2(n){var two = 2; return n + two}
 
 var funkyFunction = function() {
   return function() {
